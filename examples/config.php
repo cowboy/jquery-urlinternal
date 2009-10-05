8279730f2469567aa32beedaaf08a3d5cebae570
Ben Alman <cowboy@rj3.net>
2009-10-05 02:26:27 +0000

    not done yet, needs to be tested more

---
+++ examples/config.php
@@ -0,0 +1,19 @@
+<?PHP
+
+$shell['title1'] = "jQuery urlInternal";
+$shell['link1']  = "http://benalman.com/projects/jquery-urlinternal-plugin/";
+
+ob_start();
+?>
+  <a href="http://benalman.com/projects/jquery-urlinternal-plugin/">Project Home</a>,
+  <a href="http://benalman.com/code/projects/jquery-urlinternal/docs/">Documentation</a>,
+  <a href="http://github.com/cowboy/jquery-urlinternal/">Source</a>
+<?
+$shell['h3'] = ob_get_contents();
+ob_end_clean();
+
+$shell['jquery'] = 'jquery-1.3.2.js';
+
+$shell['shBrush'] = array( 'JScript' );
+
+?>
